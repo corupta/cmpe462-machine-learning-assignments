7262c0d66824ca0a13dd4cd0539e94f78ad673d5
corupta <halitozsoy1584@gmail.com>
2021-05-18 05:32:57 +0300

    init assignment 2 from assignment 1

---
+++ assignment2/Readme.md
@@ -0,0 +1,190 @@
+# CMPE 462 Assignment 1 Report
+
+## Part 1
+
+Samples are randomly chosen from `[-100,100)` range for `x` and `[-300,300)` range for `y`. 
+Range of y is 3 times that of x, in order to make sure the final plot looks full 
+while the target function plot line is fully visible for that range.
+ 
+Random seed can be set by changing the global variable `RANDOM_SEED`
+By default it is kept as None (numpy requests a random seed from os)
+
+Below are the results and plots for each step with `RANDOM_SEED=12345` 
+
+### Step 1
+```
+Finished calculation in 25 iterations
+Calculated weights are: [  0.         531.02365676 197.25030511]
+Decision Boundary is y = -2.69x + -0.00
+```
+![part1_step1.png](part1_step1.png "Part1 Step1 Plot")
+
+\
+&nbsp;
+
+### Step 2
+```
+Finished calculation in 119 iterations
+Calculated weights are: [ -50.         1122.35262344  371.32963463]
+Decision Boundary is y = -3.02x + 0.13
+```
+![part1_step2.png](part1_step2.png "Part1 Step2 Plot")
+
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+\
+&nbsp;
+### Step 3
+```
+Finished calculation in 23867 iterations
+Calculated weights are: [-6418.         20547.05277251  6856.03386458]
+Decision Boundary is y = -3.00x + 0.94
+```
+![part1_step3.png](part1_step3.png "Part1 Step3 Plot")
+    
+### Part 1 Overview
+
+When we check out above results, we can see 
+that the decision boundary gets closer to the original target function 
+as the number of sampled points are increased.
+
+Also, as the number of sampled points are increased, 
+PLA needs to make much more iterations to fully converge.
+
+See below table for comparing above results.
+
+
+|Step| Samples | Iterations |y = -3x + 1|
+|--|--|--|--|
+|1|50|25|y = -2.69x + -0.00|
+|2|100|119|y = -3.02x + 0.13|
+|3|5000|23867|y = -3.00x + 0.94|
+
+
+## Part 2
+
+Implemented closed form solution, so there will not be any loss over iterations graphs. 
+
+Chose to apply 5-fold cross validation, 
+this can be changed via the global variable, `PART2_S_FOLD_S_VALUE` 
+
+Created a few supplementary graphs in order to get a better grasp of how well it predicts, and which lambda value to choose.
+
+Timing includes cross validation, but none of the extra-supplementary graph generations.
+
+Set `PART2_PLOT_PREDICTIONS=True` to create `part2_stepN_predictions.png` which 
+shows predictions vs actual target values for each sample.
+
+Set `PART2_PLOT_LAMBDA_VALUES=True` to create `part2_stepN_regularization.png`
+a graph of training & testing results (root mean square errors) over varying ln-lambda value. 
+This will be same for step2 and step3 as expected, because they are only varied by regularization. 
+
+Chosen the lambda value for regularization as `e^-10` yet 
+it is neither beneficial nor harmful, as it seems. 
+
+When we choose to do 2-fold cross validation instead, 
+regularization becomes useful because of tolerating 
+the lack of many training values causing over-fitting. 
+But such analysis is out of this report's scope. 
+If interested, one can set `PART2_S_FOLD_S_VALUE=2` and 
+`PART2_PLOT_LAMBDA_VALUES=True` and run the program on part2, 
+and check out the generated graph, `part2_stepN_regularization.png`.   
+
+\
+\
+\
+\
+\
+&nbsp;
+
+### Step 1
+```
+Applying 5-fold cross validation
+There were 100 independent variables and 1 dependent variables
+There were 1000 samples in total
+Completed in 3.160 milliseconds
+Average Erms_train = 36.257771814312136
+Average Erms_test = 41.3584787667631
+```
+![part2_step1_predictions.png](part2_step1_predictions.png "Part2 Step1 Predictions Plot")
+![part2_step1_regularization.png](part2_step1_regularization.png "Part2 Step1 Regularization Plot")
+### Step 2
+```
+Applying 5-fold cross validation
+There were 500 independent variables and 1 dependent variables
+There were 1000 samples in total
+Completed in 48.580 milliseconds
+Average Erms_train = 23.6657974544233
+Average Erms_test = 65.28337321386573
+```
+![part2_step2_predictions.png](part2_step2_predictions.png "Part2 Step2 Predictions Plot")
+![part2_step2_regularization.png](part2_step2_regularization.png "Part2 Step2 Regularization Plot")
+### Step 3
+```
+Applying 5-fold cross validation
+There were 500 independent variables and 1 dependent variables
+There were 1000 samples in total
+Completed in 52.595 milliseconds
+Average Erms_train = 23.665797456855017
+Average Erms_test = 65.28335649367837
+```
+![part2_step3_predictions.png](part2_step3_predictions.png "Part2 Step3 Predictions Plot")
+![part2_step3_regularization.png](part2_step3_regularization.png "Part2 Step3 Regularization Plot")
+
+
+### Part 2 Overview
+
+Since I implemented the close form solution, the time it takes mostly depends on 
+the matrix multiplication operations applied to the input. Thus, time spent depends on 
+the input size and how long constant number of matrix multiplication operations 
+on that input takes. Matrix multiplication can be considered `O(N^2)` where N resembles 
+the size of the whole input (not one dimension) 
+Input size is `100 * 1000 = 100k` for step 1, and `500 * 1000 = 500k` for step 2-3. Since, `500k/100k = 5` and `5^2 = 25`, 
+I would expect step 2/3 to take 25 times that of step 1. 
+We can see in the below table a similar fashion can be observed. 
+(The time spent varies across different runs but the ratio of time spent on step 1 vs 2/3 is around 25 as expected)
+
+When we check out the Erms values of step 2 and 3, we can see that the regularization did not do any change. 
+Can check out the regularization graphs above and deduce that choosing a higher lambda value doesn't perform better, and if chosen too big, causes under-fitting.
+
+When we check out the Erms values of step 1 vs 2/3, we can see that step 1 performs worse on training result. 
+That is probably related to the ratio of the number of independent variables to sample sizes. 
+That's because unless there was a clear linear generation method for data, 
+fitting a linear regression line gets harder to cover for samples as the sample size gets higher than the number of independent variables.
+So maybe, applying a polynomial/logistic regression to step 1 might have performed better.  
+
+When we check out the test Erms values however, step 1 actually does perform better. 
+That's probably again because of the same ratio. Since the number of independent variables and samples are close in step 2/3, 
+it has a higher chance of over-fitting, and that's why they're performing worse on test Erms.   
+
+See below tables for comparing above results.
+
+\
+\
+\
+\
+&nbsp;
+
+| Step | IV | Samples | L2 Lamda | Time (ms) | Train Erms | Test Erms |
+|--|--|--|--|--|--|--|
+| 1 | 100 | 1000 | - | 3.160 | 36.258 | 41.358 |
+| 2 | 500 | 1000 | - | 48.580 | 23.666 | 65.283 |
+| 3 | 500 | 1000 | λ = e^-10 | 52.595 | 23.665 | 65.283 |
+IV: Independent Variables
+Erms values are average of each run of s-fold cross-validation
+Time is the time spent for calculating weights including cross-validation
+Samples are the total number of samples before separating them for training and testing.
+L2 Lambda is the lambda value used for L2 Regularization if applied, `-`, otherwise.
---
+++ assignment2/assignment2.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
+import time
+import sys
+
+SHOW_PLOTS = False
+RANDOM_SEED = None
+# RANDOM_SEED = 12345
+
+rng = np.random.default_rng(RANDOM_SEED)
+
+# PART1_POINT_XY_RANGE = 200  # center is 0
+PART1_POINT_X_RANGE = 200  # center is 0
+PART1_POINT_Y_RANGE = 600  # center is 0
+
+PART2_REGULARIZATION_LAMBDA = np.exp(-10)
+PART2_S_FOLD_S_VALUE = 5
+PART2_PLOT_PREDICTIONS = False
+PART2_PLOT_LAMBDA_VALUES = False
+
+def part1(step):
+    # pick random (x,y) points where both x and y are in range [-100,100)
+    point_count = 0
+    point_size = 1
+    if step == 1:
+        point_count = 50
+        point_size = 8
+    elif step == 2:
+        point_count = 100
+        point_size = 4
+    elif step == 3:
+        point_count = 5000
+        point_size = 1
+    else:
+        raise Exception("On part1: Unexpected step " + str(step) + ", must be one of  1-3")
+    points = np.concatenate((
+        rng.random((point_count, 1)) * PART1_POINT_X_RANGE - (PART1_POINT_X_RANGE/2),
+        rng.random((point_count, 1)) * PART1_POINT_Y_RANGE - (PART1_POINT_Y_RANGE / 2),
+    ), axis=1)
+    classes = []
+    for [x, y] in points:
+        classes.append(0 if y < -3 * x + 1 else 1)
+        # assume c=1 if y = -3x+1
+    classes = np.array(classes)
+
+    # points and classes are ready now, start PLA
+    target_outputs = 2 * classes - 1  # map class 0 to -1, class 1 to 1 for PLA
+    inputs_with_dummy_x0 = np.concatenate((np.ones((point_count, 1)), points), axis=1)
+    w = np.zeros((3, 1))  # w means current weights
+    t = 1  # t means current iteration index
+    while True:
+        if t % 1000 == 1:
+            print("PLA ITERATION #{} STARTED".format(t))
+        calculated = np.matmul(inputs_with_dummy_x0, w)
+        misclassified = []
+        for i in range(point_count):
+            c = calculated[i, 0]
+            c_sign = -1 if c < 0 else 1  # assume sign of 0 is 1 (+)
+            if c_sign != target_outputs[i]:
+                misclassified.append(i)
+        if t==1000000: # prevent infinite loop just in case
+            break
+        if len(misclassified) == 0:
+            break  # complete PLA
+        chosen_misclassified_index = misclassified[rng.integers(0, len(misclassified))]
+        input_misclassfied = inputs_with_dummy_x0[chosen_misclassified_index]
+        target_output_misclassified = target_outputs[chosen_misclassified_index]
+        # update weights
+        u = np.reshape(input_misclassfied * target_output_misclassified, (3, 1))
+        w += u
+        t += 1  # next iteration
+    print("Finished calculation in {} iterations".format(t))
+    print("Calculated weights are: {}".format(w.flatten()))
+    # our decision boundary is ready:    sign ( w0 * 1 + w1 * x + w2 * y )
+    # which is  c = 1  if  w0 * 1 + w1 * x + w2 * y >= 0  else 0
+    # thus, our decision boundary is =>  y = (- w0 - w1 * x) / w2
+    # if we try to write it as y = mx + b, m = -w1/w2, b = -w0/w2
+    m = -w[1, 0] / w[2, 0]
+    b = -w[0, 0] / w[2, 0]
+
+    print('Decision Boundary is y = {:.2f}x + {:.2f}'.format(m, b))
+
+    line_x = np.linspace(- (PART1_POINT_X_RANGE/2), + (PART1_POINT_X_RANGE/2), 100)
+
+    points_color_map = mcolors.ListedColormap(["red", "blue"])
+
+    plt.plot(line_x, m * line_x + b, color='purple', label='Decision Boundary y = {:.2f}x + {:.2f}'.format(m, b))
+    plt.plot(line_x, -3 * line_x + 1, color='green', label='Target Function y = -3x + 1')
+    plt.scatter(points[:, 0], points[:, 1], s=point_size, c=classes, cmap=points_color_map)
+    plt.title('Assignment 1 Part 1 Step {} - {} points '.format(step, point_count))
+    plt.xlabel('x', color='#222222')
+    plt.ylabel('y', color='#222222')
+    plt.legend(loc='upper left')
+    plt.grid()
+    if SHOW_PLOTS:
+        plt.show()
+    plot_filename = "part1_step{}.png".format(step)
+    plt.savefig(plot_filename)
+    print("Saved the plot to {}".format(plot_filename))
+    plt.close()
+
+def solve_linear_regression(X, t, lambda_value = 0):
+    # APPLY CLOSED FORM SOLUTION
+    # w∗ = ((X'X)^-1)X't
+    # w∗ = ((X'X + λI)^−1)X't
+    w = np.matmul(
+        np.linalg.inv(
+            np.matmul(
+                np.transpose(X),
+                X
+            ) + lambda_value
+        ),
+        np.matmul(
+            np.transpose(X),
+            t
+        )
+    )
+
+    y = np.matmul(X, w)
+    #erms = np.sum((y - t) ** 2) / 2 + (
+    #    np.linalg.norm(w,2) * PART2_REGULARIZATION_LAMBDA / 2
+    #    if apply_l2_regularization else 0
+    #)
+    erms = np.sqrt(np.sum((y - t) ** 2) / t.size)
+
+    return w, erms, y
+
+def solve_linear_regression_s_fold(X, t, lambda_value = 0):
+    n, m = X.shape
+    sample_per_fold = int(n / PART2_S_FOLD_S_VALUE)
+    if sample_per_fold * PART2_S_FOLD_S_VALUE != n:
+        raise Exception("Sample count {} is not divisible by S, {} of s-fold"
+                        .format(n, PART2_S_FOLD_S_VALUE))
+    erms_test_values = np.zeros(PART2_S_FOLD_S_VALUE)
+    erms_train_values = np.zeros(PART2_S_FOLD_S_VALUE)
+    for i in range(PART2_S_FOLD_S_VALUE):
+        n_start = i * sample_per_fold
+        n_end = (i+1) * sample_per_fold
+        X_test = X[n_start:n_end, :]
+        X_train = np.concatenate((
+            X[0:n_start, :],
+            X[n_end:, :]
+        ), axis=0)
+        t_test = t[n_start:n_end, :]
+        t_train = np.concatenate((
+            t[0:n_start, :],
+            t[n_end:, :]
+        ))
+        w, erms_train, y_train = solve_linear_regression(X_train, t_train, lambda_value)
+        y_test = np.matmul(X_test, w)
+        erms_test = np.sqrt(np.sum((y_test - t_test) ** 2) / t_test.size)
+        erms_test_values[i] = erms_test
+        erms_train_values[i] = erms_train
+    erms_test_average = np.sum(erms_test_values) / PART2_S_FOLD_S_VALUE
+    erms_train_average = np.sum(erms_train_values) / PART2_S_FOLD_S_VALUE
+    return erms_train_average, erms_test_average
+
+
+def part2(step):
+    dataset_filename = ''
+    apply_l2_regularization = False
+    if step == 1:
+        dataset_filename = 'ds1.csv'
+        apply_l2_regularization = False
+    elif step == 2:
+        dataset_filename = 'ds2.csv'
+        apply_l2_regularization = False
+    elif step == 3:
+        dataset_filename = 'ds2.csv'
+        apply_l2_regularization = True
+    else:
+        raise Exception("On part2: Unexpected step " + str(step) + ", must be one of  1-3")
+    input = np.loadtxt(dataset_filename, delimiter=",", encoding="utf8")
+    n, m = input.shape
+    X = input[:, 0:(m-1)]
+    t = input[:, (m-1):]
+    start = time.time()
+    erms_train, erms_test = solve_linear_regression_s_fold(X, t, PART2_REGULARIZATION_LAMBDA if apply_l2_regularization else 0)
+    end = time.time()
+
+    print("Applying {}-fold cross validation".format(PART2_S_FOLD_S_VALUE))
+    print("There were {} independent variables and 1 dependent variables".format(m-1))
+    print("There were {} samples in total".format(n))
+    print("Completed in {:.3f} milliseconds".format((end - start)*1000))
+    print("Average Erms_train = {}".format(erms_train))
+    print("Average Erms_test = {}".format(erms_test))
+
+    if PART2_PLOT_PREDICTIONS:
+        print("Calculating without cross-validation")
+        w, erms, y = solve_linear_regression(X, t, PART2_REGULARIZATION_LAMBDA if apply_l2_regularization else 0)
+        print("Weights = {}".format(w.flatten()))
+        plt.scatter(range(n), y, s=1, color='purple', label='Predicted y')
+        plt.scatter(range(n), t, s=1, color='green', label='Target')
+        plt.plot(range(n), y - t, color='red', label='Root Mean Square Error')
+        plt.title('Assignment 1 Part 2 Step {} - Predictions - No Cross-Validation'.format(step))
+        plt.xlabel('Sample Number', color='#222222')
+        plt.ylabel('Target Value', color='#222222')
+        plt.legend(loc='upper left')
+        plt.grid()
+        if SHOW_PLOTS:
+            plt.show()
+        plot_filename = "part2_step{}_predictions.png".format(step)
+        plt.savefig(plot_filename)
+        print("Saved the plot to {}".format(plot_filename))
+        plt.close()
+
+    if PART2_PLOT_LAMBDA_VALUES:
+        print("Calculating for a range of lambda values")
+        erms_train_values = []
+        erms_test_values = []
+        ln_lambda_values = range(-60, 5)
+        for lambda_value in ln_lambda_values:
+            erms_train, erms_test = solve_linear_regression_s_fold(X, t, np.exp(lambda_value))
+            erms_train_values.append(erms_train)
+            erms_test_values.append(erms_test)
+        plt.plot(ln_lambda_values, erms_train_values, color='blue', label='Training')
+        plt.plot(ln_lambda_values, erms_test_values, color='red', label='Test')
+        plt.title('Assignment 1 Part 2 Step {} - Regularization - {}-Fold Cross-Validation'.format(step, PART2_S_FOLD_S_VALUE))
+        plt.xlabel('ln(lambda)', color='#222222')
+        plt.ylabel('Root Mean Square Error', color='#222222')
+        plt.legend(loc='upper left')
+        plt.grid()
+        if SHOW_PLOTS:
+            plt.show()
+        plot_filename = "part2_step{}_regularization.png".format(step)
+        plt.savefig(plot_filename)
+        print("Saved the plot to {}".format(plot_filename))
+        plt.close()
+
+
+if __name__ == "__main__":
+    usageError = False
+    if len(sys.argv) != 3:
+        usageError = True
+    elif not sys.argv[1] in ['part1', 'part2']:
+        usageError = True
+    elif not sys.argv[2] in ['step1', 'step2', 'step3']:
+        usageError = True
+    if usageError:
+        print("""
+        Error! Please run the program as follows:
+        python3 assignment1.py (part1|part2) (step1|step2|step3)
+        eg: "python3 assignment1.py part1 step2" 
+        """)
+        exit(1)
+
+    step_number = int(sys.argv[2][4:])
+    if sys.argv[1] == 'part1':
+        part1(step_number)
+    elif sys.argv[1] == 'part2':
+        part2(step_number)
+    else:
+        raise Exception("On main: Unexpected part '" + sys.argv[1] + "', 'must be one of  part1, part2"'')
