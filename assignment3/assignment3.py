0197f59f79c7c4411b9255f6e6a811e4e8410ae6
corupta <halitozsoy1584@gmail.com>
2021-06-02 04:14:59 +0300

    some preparation for part1

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -2,218 +2,69 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-import math
 import numpy as np
 import time
 import sys
-import os
+
+PRINT_USEFUL_EXTRA_TEXT = True
 
 SHOW_PLOTS = False
+RANDOM_SEED = None
+# RANDOM_SEED = 12345
 
-S_FOLD_S_VALUE = 5
-MIN_LOSS_CHANGE_FOR_STOP = 0.00001
-STEP_SIZE_DEFAULT = 0.000001
+rng = np.random.default_rng(RANDOM_SEED)
 
-def sigmoid(x):
-    return 1.0 / (1.0 + np.exp(-x))
+PART1_MAX_TREE_DEPTH = 6  # max 5 splits / leaf is level 6 at max
 
-def solve_logistic_regression(X, t, batch_size = np.inf, step_size=STEP_SIZE_DEFAULT):
-    prev_loss = np.inf
-    current_loss = np.inf
-    current_iteration = 0
-    n, m = X.shape
-    loss_list = []
-    w = np.zeros((m, 1))
-    while prev_loss - (current_loss if current_loss != np.inf else 0) > MIN_LOSS_CHANGE_FOR_STOP:
-    # while current_loss > MIN_LOSS_CHANGE_FOR_STOP:
-        current_iteration += 1
-        prev_loss = current_loss
-        current_loss = 0
-        # print("Running iteration {}, prev_lost={}".format(current_iteration, prev_loss))
-        for i in range(0, n, batch_size if batch_size != np.inf else n):
-            chosen_indexes = [index for index in range(i, min(i+batch_size, n))]
-            elems = np.take(X, chosen_indexes, axis=0)
-            targets = np.take(t, chosen_indexes, axis=0)
-            # loss = sum ( ln(1 + exp (−y * wT * x)) ) / N
-            # but in our case X is not vector but a matrix (one dim is each input row)
-            # and y is not scalar but a vector (y for all input rows)
-            # thus, use below slightly modified, short version
-            a = np.matmul(
-                                elems,
-                                w
-                            )
-            b = - np.multiply(
-                            targets,
-                            np.matmul(
-                                elems,
-                                w
-                            )
-                        )
-            c = 1 + np.exp(
-                        - np.multiply(
-                            targets,
-                            np.matmul(
-                                elems,
-                                w
-                            )
-                        )
-                    )
-            d = np.log(
-                    1 + np.exp(
-                        - np.multiply(
-                            targets,
-                            np.matmul(
-                                elems,
-                                w
-                            )
-                        )
-                    )
-                )
-            batch_loss = np.sum(
-                np.log(
-                    1 + np.exp(
-                        - np.multiply(
-                            targets,
-                            np.matmul(
-                                elems,
-                                w
-                            )
-                        )
-                    )
-                )
-            ) / n
-            current_loss += batch_loss
-            # calculate gradient (modified formula to work on batch_size elements)
-            g = - np.dot(
-                elems.transpose(),
-                np.multiply(
-                    targets,
-                    sigmoid(
-                        - np.multiply(
-                            targets,
-                            np.matmul(
-                                elems,
-                                w
-                            )
-                        )
-                    )
-                )
-            ) / targets.size
-            # update weights
-            w = w - step_size * g
-        loss_list.append(current_loss)
-    return w, current_iteration, loss_list
+PART2_REGULARIZATION_LAMBDA = np.exp(-10)
+PART2_S_FOLD_S_VALUE = 5
+PART2_PLOT_PREDICTIONS = False
+PART2_PLOT_LAMBDA_VALUES = False
+
+number_to_class = ["Iris-setosa", "Iris-versicolor"]
+class_to_number = {}
+for i, class_name in enumerate(number_to_class):
+    class_to_number[class_name] = i
+
+def solve_decision_tree(X, t, use_gain_ratio):
+    # first 40 of each class
+    X_train = np.concatenate(X[0:40, :], X[50:90, :], axis=0)
+    t_train = np.concatenate(t[0:40, :], t[50:90, :], axis=0)
+    # last 10 of each class
+    X_test = np.concatenate(X[40:50, :], X[90:100, :], axis=0)
+    t_test = np.concatenate(t[40:50, :], t[90:100, :], axis=0)
+    # TODO CALCULATE DT
+    # TODO CALCULATE ACCURACY
 
 
-metrics_path = 'metrics'
-plots_path = 'plots'
-def solve_logistic_regression_s_fold(X, t, batch_size = np.inf, step_size=STEP_SIZE_DEFAULT):
-    n, m = X.shape
-    sample_per_fold = int(n / S_FOLD_S_VALUE)
-    # if sample_per_fold * S_FOLD_S_VALUE != n:
-    #    raise Exception("Sample count {} is not divisible by S, {} of s-fold"
-    #                    .format(n, S_FOLD_S_VALUE))
-    err_test_values = np.zeros(S_FOLD_S_VALUE)
-    err_train_values = np.zeros(S_FOLD_S_VALUE)
-    iteration_count_values = np.zeros(S_FOLD_S_VALUE)
-    if not os.path.exists(plots_path):
-        os.makedirs(plots_path)
-    for i in range(S_FOLD_S_VALUE):
-        n_start = i * sample_per_fold
-        n_end = (i+1) * sample_per_fold
-        X_test = X[n_start:n_end, :]
-        X_train = np.concatenate((
-            X[0:n_start, :],
-            X[n_end:, :]
-        ), axis=0)
-        t_test = t[n_start:n_end, :]
-        t_train = np.concatenate((
-            t[0:n_start, :],
-            t[n_end:, :]
-        ))
-        print("Running for Fold #{}".format(i+1))
-        w, iteration_count, loss_list = solve_logistic_regression(X_train, t_train, batch_size, step_size)
-        print("Finished in {} iterations".format(iteration_count))
-        y_train = sigmoid(np.matmul(X_train, w))
-        y_test = sigmoid(np.matmul(X_test, w))
-        err_train = np.sum((y_train - (1 + t_train)/2)**2) / t_train.size
-        err_test = np.sum((y_test - (1 + t_test)/2)**2) / t_test.size
-        err_train_values[i] = err_train
-        err_test_values[i] = err_test
-        iteration_count_values[i] = iteration_count
+def part1(step):
+    use_gain_ratio = False
+    if step == 1:
+        use_gain_ratio = False
+    elif step == 2:
+        use_gain_ratio = True
+    else:
+        raise Exception("On part1: Unexpected step " + str(step) + ", must be one of  1-2")
 
-        # create plot
-        plot_filename = "batch-{}_step-{}_fold-{}.png".format(batch_size, step_size, i+1)
-        plot_filepath = os.path.join(plots_path, plot_filename)
-        plt.plot(range(iteration_count), loss_list, color='red', label='Loss per iteration')
-        # plt.scatter(range(iteration_count), loss_list, s=1, c="red")
-        plt.title('Assignment 2 Part 1 Batch Size {} - Step Size {} - Fold #{}'.format(batch_size, step_size, i+1))
-        plt.xlabel('iterations', color='#222222')
-        plt.ylabel('loss', color='#222222')
-        plt.legend(loc='upper left')
-        plt.grid()
-        if SHOW_PLOTS:
-            plt.show()
-        plt.savefig(plot_filepath)
-        print("Saved the plot to {}".format(plot_filepath))
-        plt.close()
-    err_test_average = np.sum(err_test_values) / S_FOLD_S_VALUE
-    err_train_average = np.sum(err_train_values) / S_FOLD_S_VALUE
-
-    return err_train_average, err_test_average, err_train_values, err_test_values, iteration_count_values
-# van or not?
-target_name_to_number = {
-    "van": 1,
-    "saab": -1
-}
-def part1(batch_size, step_size = STEP_SIZE_DEFAULT):
-    dataset_filename = 'vehicle.csv'
-    input = np.loadtxt(dataset_filename, delimiter=",", encoding="utf8", skiprows=1, dtype=str)
-    # select those with target_classes only
-    input = input[(input[:, -1] == "saab") | (input[:, -1] == "van")]
+    dataset_filename = 'iris.csv'
+    input = np.loadtxt(dataset_filename, delimiter=",", encoding="utf8", skiprows=0, dtype=str)
+    headings = input[0, :]
+    input = input[1:, :]
     n, m = input.shape
-    X = input[:, 0:(m-1)].astype(np.float)
-    t = np.vectorize(target_name_to_number.get)(input[:, (m-1):])
+    X = input[:, 0:(m - 1)].astype(np.float)
+    t = np.vectorize(class_to_number.get)(input[:, (m - 1):])
+
     start = time.time()
-    err_train_average, err_test_average, err_train_values, err_test_values, iteration_count_values = solve_logistic_regression_s_fold(X, t, batch_size, step_size)
+    accuracy, root_param_index = solve_decision_tree(
+        X, t, use_gain_ratio)
     end = time.time()
 
-    print("Applying {}-fold cross validation".format(S_FOLD_S_VALUE))
-    print("There were {} independent variables and 1 dependent variables".format(m - 1))
-    print("There were {} samples in total".format(n))
-    print("Completed in {:.3f} milliseconds".format((end - start) * 1000))
+    if PRINT_USEFUL_EXTRA_TEXT:
+        print("calculated in {:.3f} ms".format((end - start)*1000))
+    root_param = headings[root_param_index]
+    print("DT {} {:.2f}".format(root_param, accuracy))
+    return
 
-    if not os.path.exists(metrics_path):
-        os.makedirs(metrics_path)
-    metric_filename = "batch-{}_step-{}.txt".format(batch_size, step_size)
-    with open(os.path.join(metrics_path, metric_filename), 'w') as f:
-        lines = [
-            "Average err_train = {}".format(err_train_average),
-            "Average err_test = {}".format(err_test_average),
-            "Stats by Fold:"
-        ]
-        for i in range(S_FOLD_S_VALUE):
-            lines.append("Fold #{} : iterations = {} , err_train = {:.3f} , err_test = {:.3f}".format(
-                i+1,iteration_count_values[i], err_train_values[i], err_test_values[i]))
-        for line in lines:
-            print(line)
-        f.write("\n".join(lines))
-
-def part1_old(step):
-    # pick random (x,y) points where both x and y are in range [-100,100)
-    point_count = 0
-    point_size = 1
-    if step == 1:
-        point_count = 50
-        point_size = 8
-    elif step == 2:
-        point_count = 100
-        point_size = 4
-    elif step == 3:
-        point_count = 5000
-        point_size = 1
-    else:
-        raise Exception("On part1: Unexpected step " + str(step) + ", must be one of  1-3")
     points = np.concatenate((
         rng.random((point_count, 1)) * PART1_POINT_X_RANGE - (PART1_POINT_X_RANGE/2),
         rng.random((point_count, 1)) * PART1_POINT_Y_RANGE - (PART1_POINT_Y_RANGE / 2),
@@ -280,42 +131,155 @@ def part1_old(step):
     print("Saved the plot to {}".format(plot_filename))
     plt.close()
 
+def solve_linear_regression(X, t, lambda_value = 0):
+    # APPLY CLOSED FORM SOLUTION
+    # w∗ = ((X'X)^-1)X't
+    # w∗ = ((X'X + λI)^−1)X't
+    w = np.matmul(
+        np.linalg.inv(
+            np.matmul(
+                np.transpose(X),
+                X
+            ) + lambda_value
+        ),
+        np.matmul(
+            np.transpose(X),
+            t
+        )
+    )
 
-def calculate_all():
-    step_sizes = [0.0000001, 0.000001, 0.00001]
-    batch_sizes = [1, np.inf, 32]
-    for batch_size in batch_sizes:
-        for step_size in step_sizes:
-            print("Running for batch={}, step={}".format(batch_size, step_size))
-            part1(batch_size, step_size)
+    y = np.matmul(X, w)
+    #erms = np.sum((y - t) ** 2) / 2 + (
+    #    np.linalg.norm(w,2) * PART2_REGULARIZATION_LAMBDA / 2
+    #    if apply_l2_regularization else 0
+    #)
+    erms = np.sqrt(np.sum((y - t) ** 2) / t.size)
+
+    return w, erms, y
+
+def solve_linear_regression_s_fold(X, t, lambda_value = 0):
+    n, m = X.shape
+    sample_per_fold = int(n / PART2_S_FOLD_S_VALUE)
+    if sample_per_fold * PART2_S_FOLD_S_VALUE != n:
+        raise Exception("Sample count {} is not divisible by S, {} of s-fold"
+                        .format(n, PART2_S_FOLD_S_VALUE))
+    erms_test_values = np.zeros(PART2_S_FOLD_S_VALUE)
+    erms_train_values = np.zeros(PART2_S_FOLD_S_VALUE)
+    for i in range(PART2_S_FOLD_S_VALUE):
+        n_start = i * sample_per_fold
+        n_end = (i+1) * sample_per_fold
+        X_test = X[n_start:n_end, :]
+        X_train = np.concatenate((
+            X[0:n_start, :],
+            X[n_end:, :]
+        ), axis=0)
+        t_test = t[n_start:n_end, :]
+        t_train = np.concatenate((
+            t[0:n_start, :],
+            t[n_end:, :]
+        ))
+        w, erms_train, y_train = solve_linear_regression(X_train, t_train, lambda_value)
+        y_test = np.matmul(X_test, w)
+        erms_test = np.sqrt(np.sum((y_test - t_test) ** 2) / t_test.size)
+        erms_test_values[i] = erms_test
+        erms_train_values[i] = erms_train
+    erms_test_average = np.sum(erms_test_values) / PART2_S_FOLD_S_VALUE
+    erms_train_average = np.sum(erms_train_values) / PART2_S_FOLD_S_VALUE
+    return erms_train_average, erms_test_average
+
+
+def part2(step):
+    dataset_filename = ''
+    apply_l2_regularization = False
+    if step == 1:
+        dataset_filename = 'ds1.csv'
+        apply_l2_regularization = False
+    elif step == 2:
+        dataset_filename = 'ds2.csv'
+        apply_l2_regularization = False
+    elif step == 3:
+        dataset_filename = 'ds2.csv'
+        apply_l2_regularization = True
+    else:
+        raise Exception("On part2: Unexpected step " + str(step) + ", must be one of  1-3")
+    input = np.loadtxt(dataset_filename, delimiter=",", encoding="utf8")
+    n, m = input.shape
+    X = input[:, 0:(m-1)]
+    t = input[:, (m-1):]
+    start = time.time()
+    erms_train, erms_test = solve_linear_regression_s_fold(X, t, PART2_REGULARIZATION_LAMBDA if apply_l2_regularization else 0)
+    end = time.time()
+
+    print("Applying {}-fold cross validation".format(PART2_S_FOLD_S_VALUE))
+    print("There were {} independent variables and 1 dependent variables".format(m-1))
+    print("There were {} samples in total".format(n))
+    print("Completed in {:.3f} milliseconds".format((end - start)*1000))
+    print("Average Erms_train = {}".format(erms_train))
+    print("Average Erms_test = {}".format(erms_test))
+
+    if PART2_PLOT_PREDICTIONS:
+        print("Calculating without cross-validation")
+        w, erms, y = solve_linear_regression(X, t, PART2_REGULARIZATION_LAMBDA if apply_l2_regularization else 0)
+        print("Weights = {}".format(w.flatten()))
+        plt.scatter(range(n), y, s=1, color='purple', label='Predicted y')
+        plt.scatter(range(n), t, s=1, color='green', label='Target')
+        plt.plot(range(n), y - t, color='red', label='Root Mean Square Error')
+        plt.title('Assignment 1 Part 2 Step {} - Predictions - No Cross-Validation'.format(step))
+        plt.xlabel('Sample Number', color='#222222')
+        plt.ylabel('Target Value', color='#222222')
+        plt.legend(loc='upper left')
+        plt.grid()
+        if SHOW_PLOTS:
+            plt.show()
+        plot_filename = "part2_step{}_predictions.png".format(step)
+        plt.savefig(plot_filename)
+        print("Saved the plot to {}".format(plot_filename))
+        plt.close()
+
+    if PART2_PLOT_LAMBDA_VALUES:
+        print("Calculating for a range of lambda values")
+        erms_train_values = []
+        erms_test_values = []
+        ln_lambda_values = range(-60, 5)
+        for lambda_value in ln_lambda_values:
+            erms_train, erms_test = solve_linear_regression_s_fold(X, t, np.exp(lambda_value))
+            erms_train_values.append(erms_train)
+            erms_test_values.append(erms_test)
+        plt.plot(ln_lambda_values, erms_train_values, color='blue', label='Training')
+        plt.plot(ln_lambda_values, erms_test_values, color='red', label='Test')
+        plt.title('Assignment 1 Part 2 Step {} - Regularization - {}-Fold Cross-Validation'.format(step, PART2_S_FOLD_S_VALUE))
+        plt.xlabel('ln(lambda)', color='#222222')
+        plt.ylabel('Root Mean Square Error', color='#222222')
+        plt.legend(loc='upper left')
+        plt.grid()
+        if SHOW_PLOTS:
+            plt.show()
+        plot_filename = "part2_step{}_regularization.png".format(step)
+        plt.savefig(plot_filename)
+        print("Saved the plot to {}".format(plot_filename))
+        plt.close()
 
 
 if __name__ == "__main__":
-    # calculate_all()
-    # exit(0)
     usageError = False
     if len(sys.argv) != 3:
         usageError = True
-    elif not sys.argv[1] in ['part1']:
+    elif not sys.argv[1] in ['part1', 'part2']:
         usageError = True
-    elif (not sys.argv[2] in ['step1', 'step2']) and sys.argv[2] != str(int(sys.argv[2])):
+    elif not sys.argv[2] in ['step1', 'step2']:
         usageError = True
     if usageError:
         print("""
         Error! Please run the program as follows:
-        python3 assignment1.py (part1) (step1|step2|<number>)
+        python3 assignment1.py (part1|part2) (step1|step2)
         eg: "python3 assignment1.py part1 step2" 
         """)
         exit(1)
 
-    batch_size = None
-    if sys.argv[2] == "step1":
-        batch_size = math.inf
-    elif sys.argv[2] == "step2":
-        batch_size = 1
-    else:
-        batch_size = int(sys.argv[2])
+    step_number = int(sys.argv[2][4:])
     if sys.argv[1] == 'part1':
-        part1(batch_size)
+        part1(step_number)
+    elif sys.argv[1] == 'part2':
+        part2(step_number)
     else:
-        raise Exception("On main: Unexpected part '" + sys.argv[1] + "', 'must be one of  part1"'')
+        raise Exception("On main: Unexpected part '" + sys.argv[1] + "', 'must be one of  part1, part2"'')
